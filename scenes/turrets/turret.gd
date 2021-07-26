34386f05874dd007eaf865d20d590a75cda0bda6
C D <Callu@Icarus.local>
2021-07-26 18:05:49 +0100

    Initial commit

---
+++ scenes/turrets/turret.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _physics_process(_delta):
+	turn()
+	
+func turn():
+	var enemy_position = get_global_mouse_position()
+	#get_node("	
+	#get_node("gunt1").look_at(enemy_position)
+	#get_node("turret").look_at(enemy_position)
+	
+
+
